ba1f278273eadcc1088b37297c85e15937be3889
Alex Moore <moore.alex@gmail.com>
2011-10-31 18:09:31 -0400

    First commit, for funsies

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Timer = System.Timers.Timer;
+
+namespace SpaceHeater
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            const int interval = 5 * 1000 * 60;
+            var timer = new Timer(interval);
+            var cts = new CancellationTokenSource();
+            timer.Elapsed += (sender, args) =>
+                                 {
+                                     Console.WriteLine("Time is up");
+                                     cts.Cancel();
+                                     Environment.Exit(0);
+                                 };
+
+            var options = new ParallelOptions
+                              {
+                                  MaxDegreeOfParallelism = (int)Math.Ceiling(0.75 * Environment.ProcessorCount),
+                                  CancellationToken = cts.Token
+                              };
+
+            Task.Factory.StartNew(() =>
+                                              {
+                                                  try
+                                                  {
+                                                      // Useless work
+                                                      Parallel.For(long.MinValue + 1, long.MaxValue, options,
+                                                                   i => { i--; });
+                                                  }
+                                                  catch (OperationCanceledException)
+                                                  {
+                                                  }
+                                              },
+                                          cts.Token);
+
+            timer.Start();
+
+            Console.WriteLine("Press any key to cancel...");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Space Heater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Enginerding")]
+[assembly: AssemblyProduct("Space Heater")]
+[assembly: AssemblyCopyright("Copyright © 2011 Alex Moore")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6da3585b-05cf-491c-bf4b-a165122b630b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
